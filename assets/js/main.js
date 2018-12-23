16c20b04a4b9da6d4045e9f87ba9cd39e8f6e19f
Tom Pichette <pichettet1@gmail.com>
2018-12-23 14:14:01 -0500

    Changed scroll highlight color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-    
+
     /* ======= Fixed header when scrolled ======= */
-    
+
     $(window).bind('scroll', function() {
          if ($(window).scrollTop() > 0) {
              $('#header').addClass('header-scrolled');
@@ -11,24 +11,24 @@ $(document).ready(function() {
              $('#header').removeClass('header-scrolled');
          }
     });
-    
+
     /* ======= Scrollspy ======= */
     $('body').scrollspy({ target: '#header', offset: 100});
-    
+
     /* ======= ScrollTo ======= */
     $('a.scrollto').on('click', function(e){
-        
+
         //store hash
         var target = this.hash;
-                
+
         e.preventDefault();
-        
+
 		$('body').scrollTo(target, 800, {offset: -50, 'axis':'y'});
         //Collapse mobile menu after clicking
 		if ($('.navbar-collapse').hasClass('show')){
 			$('.navbar-collapse').removeClass('show');
 		}
-		
+
 	});
 
 
